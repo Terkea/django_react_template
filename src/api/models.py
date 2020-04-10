78752c45552174f8c673f7631c3d4c34ace4f200
Terkea <terkeabt@gmail.com>
2020-04-10 14:18:21 +0100

    api | custom profile | buggy tho

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,11 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # custom fields for user
-    company_name = models.CharField(max_length=100, null=True)
+    address = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=20, null=True)
+    postcode = models.CharField(max_length=10, null=True)
+    mobile_phone = models.CharField(max_length=20, null=True)
+    avatar = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.user.username
--- src/api/serializers.py
+++ src/api/serializers.py
@@ -5,22 +5,58 @@ from rest_auth.serializers import UserDetailsSerializer
 
 class UserSerializer(UserDetailsSerializer):
     # not required field
-    company_name = serializers.CharField(source="userprofile.company_name", allow_null=True, allow_blank=True, default=None)
+    address = serializers.CharField(source="userprofile.address", allow_null=True, allow_blank=True, default=None)
+    city = serializers.CharField(source="userprofile.city", allow_null=True, allow_blank=True, default=None)
+    postcode = serializers.CharField(source="userprofile.postcode", allow_null=True, allow_blank=True, default=None)
+    mobile_phone = serializers.CharField(source="userprofile.mobile_phone", allow_null=True, allow_blank=True, default=None)
+    avatar = serializers.CharField(source="userprofile.avatar", allow_null=True, allow_blank=True, default=None)
 
     class Meta(UserDetailsSerializer.Meta):
-        fields = UserDetailsSerializer.Meta.fields + ('company_name',)
+        fields = UserDetailsSerializer.Meta.fields + ('address',)
+        fields = UserDetailsSerializer.Meta.fields + ('city',)
+        fields = UserDetailsSerializer.Meta.fields + ('postcode',)
+        fields = UserDetailsSerializer.Meta.fields + ('mobile_phone',)
+        fields = UserDetailsSerializer.Meta.fields + ('avatar',)
 
     def update(self, instance, validated_data):
         # disable update username when updating the user
         username = validated_data.pop('username', None)
 
         profile_data = validated_data.pop('userprofile', {})
-        company_name = profile_data.get('company_name')
+
+        # custom fields
+        address = profile_data.get('address')
+        city = profile_data.get('city')
+        postcode = profile_data.get('postcode')
+        mobile_phone = profile_data.get('mobile_phone')
+        avatar = profile_data.get('avatar')
+        
         instance = super(UserSerializer, self).update(instance, validated_data)
 
         # get and update user profile
-        profile = instance.userprofile
-        if profile_data and company_name:
-            profile.company_name = company_name
+        profile = instance.address
+        if profile_data and address:
+            profile.address = address
             profile.save()
+
+        profile = instance.city
+        if profile_data and city:
+            profile.city = city
+            profile.save()
+
+        profile = instance.postcode
+        if profile_data and postcode:
+            profile.postcode = postcode
+            profile.save()
+
+        profile = instance.mobile_phone
+        if profile_data and mobile_phone:
+            profile.mobile_phone = mobile_phone
+            profile.save()
+
+        profile = instance.avatar
+        if profile_data and avatar:
+            profile.avatar = avatar
+            profile.save()
+
         return instance
