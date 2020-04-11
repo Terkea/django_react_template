e52630134d9d5ea9a8f8b818bcfd5d811287af95
unknown <vadim444@hotmail.com>
2020-04-11 16:06:40 +0100

    fixed layout

--- react/src/containers/Layout.js
+++ react/src/containers/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
-import { Link, withRouter } from 'react-router-dom';
+import { Link, withRouter, useLocation } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../store/actions/auth';
 
 const { Header, Content, Footer } = Layout;
 
 const CustomLayout = props => {
+    const { pathname } = useLocation()
+
     return (
         <Layout className="layout" style={{ minHeight: '100vh', overflow: "auto" }}>
             <Header>
@@ -14,21 +16,22 @@ const CustomLayout = props => {
                 <Menu
                     theme="dark"
                     mode="horizontal"
-                    defaultSelectedKeys={['1']}
+                    selectedKeys={[pathname]}
                     style={{ lineHeight: '64px' }}
                 >
-                    <Menu.Item key="1">
+
+                    <Menu.Item key="/">
                         <Link to="/">Home</Link>
                     </Menu.Item>
 
                     {
                         props.isAuthenticated ?
-                            <Menu.Item key="2" onClick={props.logout}>
+                            <Menu.Item key="/login/" onClick={props.logout}>
                                 Logout
-                        </Menu.Item>
+                            </Menu.Item>
                             :
-                            <Menu.Item key="2">
-                                <Link to="/login">Login</Link>
+                            <Menu.Item key="/login/">
+                                <Link to="/login/">Login</Link>
                             </Menu.Item>
                     }
 
@@ -36,15 +39,15 @@ const CustomLayout = props => {
                         props.isAuthenticated ?
                             null
                             :
-                            < Menu.Item key="3">
-                                <Link to="/signup">Sign up</Link>
+                            < Menu.Item key="/signup/">
+                                <Link to="/signup/">Sign up</Link>
                             </Menu.Item>
                     }
 
                 </Menu>
             </Header>
             <Content style={{ padding: '0 50px' }}>
-                <div style={{padding: 24 }}>
+                <div style={{ padding: 24 }}>
                     {props.children}
                 </div>
             </Content>
