a50037b41673d7ed666677cb08159db4ca7adba5
Terkea <terkeabt@gmail.com>
2020-04-12 18:56:51 +0100

    styled 404 component

--- react/src/containers/Errors/E404.js
+++ react/src/containers/Errors/E404.js
@@ -1,17 +1,37 @@
 import React from 'react'
-import { Result, Button } from 'antd';
+import { Result, Button, Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
 
+const styles = {
+    svgBackground: {
+        backgroundImage: "url(" + 'https://gw.alipayobjects.com/zos/rmsportal/TVYTbAXWheQpRcWDaDMu.svg)',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center 110px',
+        backgroundSize: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        overflow: 'auto'
+    }
+}
 
-export default function Error_404() {
+const Error_404 = () => {
     return (
-        <div>
-            <Result
-                status="404"
-                title="404"
-                subTitle="Sorry, the page you visited does not exist."
-                extra={<Button type="primary"><Link to='/'>Back Home</Link></Button>}
-            />
+        <div style={styles.svgBackground}>
+            <Row type="flex" justify="center" align="middle" style={{ height: '100vh' }}>
+                <Col span={24}>
+                    <Result
+                        // style={{horizontal}}
+                        status="404"
+                        title="404"
+                        subTitle="Sorry, the page you visited does not exist."
+                        extra={<Button type="primary"><Link to='/'>Back Home</Link></Button>}
+
+                    />
+                </Col>
+            </Row>
         </div>
     )
 }
+
+export default Error_404
