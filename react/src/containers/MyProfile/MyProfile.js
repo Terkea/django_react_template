176ce97e78caca576a3f01fd600a5df0b4e2f696
unknown <vadim444@hotmail.com>
2020-04-12 20:14:33 +0100

    Modified My_Profile

--- react/src/containers/MyProfile/MyProfile.js
+++ react/src/containers/MyProfile/MyProfile.js
@@ -1,5 +1,5 @@
 import React, { useState, Fragment } from 'react';
-import { Row, Col, Typography, Divider, List, Menu } from 'antd';
+import { Row, Col, Typography, Layout, Menu } from 'antd';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import BasicSettings from './Settings/Basic';
 import SecuritySettings from './Settings/Security';
@@ -11,6 +11,7 @@ import { NavLink } from 'react-router-dom'
 
 // destructure the props
 const { Title } = Typography;
+const { Content } = Layout;
 
 const styles = {
     heightForTheRow: {
@@ -21,6 +22,14 @@ const styles = {
     }
 }
 
+const menuPagetitle = {
+    // marginBottom: '12px',
+    // color: 'rgba(0, 0, 0, .85)',
+    fontWeight: '500',
+    // paddingLeft: 10,
+    fontSize: '1em',
+    // lineHeight: '28px',
+}
 
 
 const MyProfile = (props) => {
@@ -37,35 +46,43 @@ const MyProfile = (props) => {
     return (
         <div>
             <Title justify="center" align="middle" style={styles.titleStyle}>My account</Title>
-            <Row type="flex" style={styles.heightForTheRow}>
-                <Col span={4}>
-                    <Menu
-                        // onClick={handleClick}
-                        style={{ width: '100%' }}
-                        defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
-                        mode="inline"
-                    >
-                        <Menu.Item style={styles.menuItem} key="1">
-                            <Link to={`${getUrl()}basic/`}>Basic Settings</Link>
-                        </Menu.Item>
-                        <Menu.Item style={styles.menuItem} key="2">
-                            <Link to={`${getUrl()}security/`}>Security Settings</Link>
-                        </Menu.Item>
-                    </Menu>
-                </Col>
-                <Col span={18} style={{ width: '100%' }}>
-                    <Switch>
-                        {/* <Route exact path="/" component={Login} /> */}
-                        <Route exact path={`${getUrl()}basic/`} component={BasicSettings} />
-                        <Route exact path={`${getUrl()}security/`} component={SecuritySettings} />
-                    </Switch>
-                </Col>
-            </Row>
+            <Content
+                style={{
+                    padding: 24,
+                    margin: 0,
+                    minHeight: 280,
+                    background: '#fff',
+                }}
+            >
+                <Row type="flex" style={styles.heightForTheRow}>
+                    <Col span={4}>
+                        <Menu
+                            // onClick={handleClick}
+                            style={{ width: '100%' }}
+                            defaultSelectedKeys={['1']}
+                            defaultOpenKeys={['sub1']}
+                            mode="inline"
+                        >
+                            <Menu.Item style={styles.menuItem} key="1">
+                                <Link to={`${getUrl()}basic/`}><div style={menuPagetitle}>Basic Settings</div></Link>
+                            </Menu.Item>
+                            <Menu.Item style={styles.menuItem} key="2">
+                                <Link to={`${getUrl()}security/`}><div style={menuPagetitle}>Security Settings</div></Link>
+                            </Menu.Item>
+                        </Menu>
+                    </Col>
+                    <Col span={18} style={{ width: '100%' }}>
+                        <Switch>
+                            {/* <Route exact path="/" component={Login} /> */}
+                            <Route exact path={`${getUrl()}basic/`} component={BasicSettings} />
+                            <Route exact path={`${getUrl()}security/`} component={SecuritySettings} />
+                        </Switch>
+                    </Col>
+                </Row>
+            </Content>
         </div>
     );
 };
-
 
 
 export default connect(null, null)(MyProfile);
